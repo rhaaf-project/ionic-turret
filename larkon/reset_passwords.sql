f442a2fc7438ed314f4f480911ef45815fc63a10
rhaaf <rhaaf.project@gmail.com>
2026-01-05 18:30:28 +0700

    Fix SIP call routing and add password reset scripts - Fixed Asterisk NAT config for WebRTC-SIP bridging - Added reset_passwords.sql for Laravel user password reset - Added various debug scripts for RTP/call troubleshooting - Reverted JsSIP to empty iceServers (SmartX pattern)

---
+++ larkon/reset_passwords.sql
@@ -0,0 +1,2 @@
+UPDATE users SET password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' WHERE email = 'admin@smartx.local';
+UPDATE users SET password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' WHERE email = 'demo1@smartx.local';
