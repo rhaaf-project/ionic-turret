e65548f51f0076aaca39c3de0a74166f778caaa1
rhaaf <rhaaf.project@gmail.com>
2026-01-22 17:47:22 +0700

    fix: add New button to Recording Server list page - Added getHeaderActions with CreateAction to ListRecordingServers

--- larkon/smartX_cms/Filament/Resources/RecordingServerResource/Pages/ListRecordingServers.php
+++ larkon/smartX_cms/Filament/Resources/RecordingServerResource/Pages/ListRecordingServers.php
@@ -1,9 +1,19 @@
 <?php
 namespace App\Filament\Resources\RecordingServerResource\Pages;
+
 use App\Filament\Resources\RecordingServerResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListRecordingServers extends ListRecords
 {
     protected static string $resource = RecordingServerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
+
